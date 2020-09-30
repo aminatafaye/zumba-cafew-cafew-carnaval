3e9a128014b58dbf6aeaee2cbb8b5d52032de252
Aminata Faye <aminata.faye@epita.fr>
2020-09-30 20:20:27 +0200

    point 3 done

--- game/shoot.js
+++ game/shoot.js
@@ -83,10 +83,20 @@ function player_falling()
     for (var i = 0; i < length; i++) {
         element = noGround[i];
 
-        var tileX = (element[0]) | 0;
-        var tileY = (element[1]) | 0;
-        var mtileX = (element[0] + sizeOfTileX) | 0;
-        var mtileY = (element[1] + sizeOfTileY) | 0;
+        if (!element)
+            {
+                var tileX = 0;
+                var tileY = 0; 
+                var mtileX = 0;
+                var mtileY = 0
+            }
+        else {
+
+        var tileX = (element[0]) ;
+        var tileY = (element[1]);
+        var mtileX = (element[0] + sizeOfTileX);
+        var mtileY = (element[1] + sizeOfTileY) ;
+        }
 
         if ((x > tileX)
             && (x < mtileX)
